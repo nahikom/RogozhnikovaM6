94428a00617b4ef4951e6c2f5c00ade147880611
nahikom <94750183+nahikom@users.noreply.github.com>
2022-10-26 20:42:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,17 +15,10 @@ $('.responsive').slick({
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 480,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
         }
       }
       // You can unslick at a given breakpoint now by adding:
